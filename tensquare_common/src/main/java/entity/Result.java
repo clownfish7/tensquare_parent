a35e1d95b2e2ac2094b8e9ea59ecf4cfd28afee3
clownfish7 <279505647@qq.com>
2019-06-19 17:54:22 +0800

    Initial commit

---
+++ tensquare_common/src/main/java/entity/Result.java
@@ -0,0 +1,65 @@
+package entity;
+
+/**
+ * @author yzy
+ * @classname Result
+ * @description TODO
+ * @create 2019-06-19 16:11
+ */
+public class Result {
+
+    private Integer code;
+    private Boolean flag;
+    private String message;
+    private Object data;
+
+    public Result() {
+    }
+
+    public Result(Integer code, Boolean flag, String message) {
+
+        this.code = code;
+        this.flag = flag;
+        this.message = message;
+    }
+
+    public Result(Integer code, Boolean flag, String message, Object data) {
+        this.code = code;
+        this.flag = flag;
+        this.message = message;
+        this.data = data;
+    }
+
+    public Integer getCode() {
+
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public Boolean getFlag() {
+        return flag;
+    }
+
+    public void setFlag(Boolean flag) {
+        this.flag = flag;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
