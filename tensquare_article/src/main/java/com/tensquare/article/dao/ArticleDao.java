33aa4cec7e3f7e4329caf270b3f8a74a9f54735f
clownfish7 <279505647@qq.com>
2019-06-21 14:32:14 +0800

    article thumbup

--- tensquare_article/src/main/java/com/tensquare/article/dao/ArticleDao.java
+++ tensquare_article/src/main/java/com/tensquare/article/dao/ArticleDao.java
@@ -4,11 +4,32 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.tensquare.article.pojo.Article;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
 /**
  * 数据访问接口
- * @author Administrator
  *
+ * @author Administrator
  */
-public interface ArticleDao extends JpaRepository<Article,String>,JpaSpecificationExecutor<Article>{
-	
+public interface ArticleDao extends JpaRepository<Article, String>, JpaSpecificationExecutor<Article> {
+
+    /**
+     * 审核
+     *
+     * @param id
+     */
+    @Modifying
+    @Query(value = "update tb_article set state = '1' where id = ?", nativeQuery = true)
+    public void examine(String id);
+
+    /**
+     * 点赞
+     *
+     * @param id
+     * @return
+     */
+    @Modifying
+    @Query(value = "update tb_article set thumbup = thumbup+1 where id = ?", nativeQuery = true)
+    public void updateThumbup(String id);
 }
