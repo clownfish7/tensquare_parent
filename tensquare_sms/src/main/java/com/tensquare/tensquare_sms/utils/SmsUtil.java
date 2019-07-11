ea9e1b48ca8204e7422598ed9b60a1c77f2a68bf
clownfish7 <279505647@qq.com>
2019-07-11 21:04:10 +0800

    user sms

---
+++ tensquare_sms/src/main/java/com/tensquare/tensquare_sms/utils/SmsUtil.java
@@ -0,0 +1,15 @@
+package com.tensquare.tensquare_sms.utils;
+
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author yzy
+ * @classname SmsUtil
+ * @description TODO
+ * @create 2019-07-11 20:38
+ */
+@Component
+public class SmsUtil {
+    // TODO 短信工具类
+}
